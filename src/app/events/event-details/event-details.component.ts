ebb4774a38d00d958056b729f1fbf3366d0c3bb8
Dereje Dessalegne <ddessalegne@relias.com>
2022-11-06 11:02:22 -0500

    Create session component and session list.

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
+import { SessionListComponent } from './session-list.component';
+import { ISession } from '../shared';
 
 @Component({
     templateUrl: './event-details.component.html',
     styles: [`
     .container { padding-left: 20px; padding-right: 20px; }
     .event-image { height: 100px; }
+    a { cursor: pointer}
     `]
 })
 
 export class EventDetailsComponent {
     event: any;
-
+    addMode: boolean;
+    filterBy: string = 'all';
+    sortBy: string = 'votes';
     constructor(
         private eventService: EventService,
         private route: ActivatedRoute){
@@ -22,4 +27,26 @@ export class EventDetailsComponent {
     ngOnInit() {
         this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
     }
+
+    addSession() {
+        this.addMode = true;
+    }
+
+    saveNewSession(session: ISession)
+    {
+        const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
+
+        session.id = nextId + 1;
+        this.event.sessions.push(session);
+        this.eventService.updateEvent(this.event);
+        this.addMode = false;
+
+        console.log(this.event);
+    }
+
+    cancelAddSession(){
+        this.addMode = false;
+    }
+
+
 }
