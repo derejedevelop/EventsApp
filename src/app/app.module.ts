b1eff2031bdd9a4fced7ee8282ceca1e18107cb1
Dereje Dessalegne <ddessalegne@relias.com>
2022-11-06 10:58:26 -0500

    Add toastr service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponenet } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { Toastr, TOASTR_TOKEN } from './common/toastr.service';
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
-import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventListResolver,
+  EventRouteActivator,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe
+} from './events/index'
+import { AuthService } from './user/auth.service';
+import { CollapisbleWellComponent } from './common/collapsible-well.component';
+
+declare let toastr: Toastr;
 @NgModule({
   declarations: [
     EventsAppComponent,
@@ -18,13 +31,38 @@ import { CreateEventComponent } from './events/create-event.component';
     EventThumbnailComponent,
     NavBarComponenet,
     EventDetailsComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapisbleWellComponent,
+    DurationPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)    
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService,
+    {
+      provide: TOASTR_TOKEN, useValue: toastr
+    },
+    EventRouteActivator,
+    EventListResolver,
+    AuthService,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}  
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty)
+  {
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+
+  return true;
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,33 @@
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { EventsListComponent } from "./events/events-list.component";
 import { Routes } from '@angular/router';
-import { CreateEventComponent } from "./events/create-event.component";
+import { Error404Component } from "./errors/404.component";
+import { ProfileComponent } from "./user/profile.component";
+import {
+    EventDetailsComponent,
+    EventListResolver,
+    EventsListComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    CreateSessionComponent
+} from './events/index'
 
 export const appRoutes: Routes = [
-    {path: 'events/new', component: CreateEventComponent},
-    {path: 'events', component: EventsListComponent},
-    {path: 'events/:id', component: EventDetailsComponent},
-    {path: '', redirectTo: '/events', pathMatch: 'full'}
-    ]
+    {
+        path: 'events/new', component: CreateEventComponent,
+        canDeactivate: ['canDeactivateCreateEvent']
+    },
+    {
+        path: 'events', component: EventsListComponent,
+        resolve: {events: EventListResolver}
+    },
+    {
+        path: 'events/:id', component: EventDetailsComponent,
+        canActivate: [EventRouteActivator]
+    },
+    {path: '404', component: Error404Component},
+    {path: '', redirectTo: '/events', pathMatch: 'full'},
+    {
+        path: 'user', 
+        loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    },
+    {path: 'events/session/new', component: CreateSessionComponent}
+]
